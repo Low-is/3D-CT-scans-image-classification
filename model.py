88847715a69db1814f97c84b21999fb1cffcbbff
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2025-07-30 15:30:12 -0500

    Update model.py

--- model.py
+++ model.py
@@ -36,3 +36,35 @@ def get_model(width=128, height=128, depth=64):
 # Build model.
 model = get_model(width=128, height=128, depth=64)
 model.summary()
+
+
+# Compile model.
+initial_learning_rate = 0.0001
+lr_schedule = keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate, decay_steps=100000, decay_rate=0.96, staircase=True
+)
+model.compile(
+    loss="categorical_crossentropy",
+    optimizer=keras.optimizers.Adam(learning_rate=lr_schedule),
+    metrics=["accuracy"],
+    run_eagerly=True,
+)
+
+
+# Define callbacks.
+checkpoint_cb = keras.callbacks.ModelCheckpoint(
+    "3d_image_classification.keras", save_best_only=True
+)
+early_stopping_cb = keras.callbacks.EarlyStopping(monitor="val_acc", patience=15, mode="max")
+
+# Train the model, doing validation at the end of each epoch
+epochs = 100
+history = model.fit(
+    x_train,
+    y_train_cat,
+    validation_data=(x_test, y_test_cat),
+    epochs=epochs,
+    shuffle=True,
+    verbose=2,
+    callbacks=[checkpoint_cb, early_stopping_cb],
+)
